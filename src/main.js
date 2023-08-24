0f8e56dcd64ca2cf4340767e90b92c1e55a338e3
shyJyt <2873704761@qq.com>
2023-08-24 11:47:39 +0800

    'first-commit'

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+
+app.mount("#app");
