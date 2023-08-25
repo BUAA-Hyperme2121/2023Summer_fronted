d92902b6b82d5a471b3106fea5982b3d52d96e17
whymecry <2924147820@qq.com>
2023-08-25 09:21:29 +0800

    25-1

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,31 +8,54 @@ const router = createRouter({
       redirect: "/index",
     },
     {
-      path: '/index',
-      name: 'index',
-      component: () => import('../views/IndexView.vue')
+      path: "/index",
+      name: "index",
+      component: () => import("../views/IndexView.vue"),
     },
     {
-      path: '/home',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      path: "/home",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
     },
     {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/RegisterView.vue')
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
     },
     {
-      path: '/project',
-      name: 'project',
-      component: () => import('../views/ProjectView.vue')
-    }
-  ]
-})
+      path: "/project",
+      redirect: "/project/designlist",
+      name: "project",
+      component: () => import("../views/ProjectView.vue"),
+      children: [
+        {
+          path: "designlist",
+          name: "designlist",
+          component: () => import("../views/project/DesignList.vue"),
+        },
+        {
+          path: "doclist",
+          name: "doclist",
+          component: () => import("../views/project/DocList.vue"),
+        },
+        {
+          path: "doc",
+          name: "doc",
+          component: () => import("../views/project/DocPage.vue"),
+        },
+        {
+          path: "design",
+          name: "design",
+          component: () => import("../views/project/DesignPage.vue"),
+        },
+      ],
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/ProjectView.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-aside
+    style="width: 15%; height: 100%; position: absolute; top: 0px; left: 0px"
+  >
+    <el-menu
+      class="el-menu-vertical-demo"
+      :default-active="this.$router.path"
+      router
+      :collapse="isCollapse"
+      style="height: 100%"
+      @mouseenter="mouseOver"
+      @mouseleave="mouseLeave"
+      @select="handleMenuSelect"
+    >
+      <el-menu-item index="/project/designlist">
+        <el-icon><EditPen /></el-icon>
+        <span>页面设计</span>
+      </el-menu-item>
+      <el-menu-item index="/project/doclist">
+        <el-icon><Document /></el-icon>
+        <span>项目文档</span>
+      </el-menu-item>
+      <el-menu-item index="/home">
+        <el-icon><CircleClose /></el-icon>
+        <span>退出项目</span>
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
+  <el-main style="width: 85%; margin-left: 10%">
+    <RouterView></RouterView>
+  </el-main>
+</template>
+<script>
+import { ref } from "vue";
+import { Document, CircleClose, EditPen } from "@element-plus/icons-vue";
+export default {
+  components: {
+    Document,
+    CircleClose,
+    EditPen,
+  },
+  setup() {
+    const isCollapse = ref(true);
+    const activeIndex = ref(1);
+    function mouseOver() {
+      isCollapse.value = false;
+    }
+    function mouseLeave() {
+      isCollapse.value = true;
+    }
+    function handleMenuSelect(index) {
+      activeIndex.value = index;
+    }
+    return {
+      isCollapse,
+      activeIndex,
+      mouseOver,
+      mouseLeave,
+      handleMenuSelect,
+    };
+  },
+};
+</script>
+<style scoped></style>
